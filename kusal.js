f2a48b9d1373617c0edf87529ec883e916cf8269
kusal senanayake <ransarakusal03@gmail.com>
2023-05-27 09:28:07 +0530

    first commit

---
+++ kusal.js
@@ -0,0 +1,61 @@
+console.log("10%3")
+VM124:1 10%3
+undefined
+console.log("10%3");
+VM132:1 10%3
+undefined
+console.log("10 % 3");
+VM146:1 10 % 3
+undefined
+console.log(10%3);
+VM172:1 1
+undefined
+console.log(2**3)
+VM234:1 8
+undefined
+let x=1;
+undefined
+let x=1;
+let y=2;
+console.log("The remainder is:" + x%y);
+console.log("The exponentiation is:" + x**y);
+VM593:3 The remainder is:1
+VM593:4 The exponentiation is:1
+undefined
+let x=3;
+let y=2;
+console.log("The remainder is:" + x%y);
+console.log("The exponentiation is:" + x**y);
+VM648:3 The remainder is:1
+VM648:4 The exponentiation is:9
+undefined
+console.log(1+3*8-2);
+VM724:1 23
+undefined
+console.log((1+3)*(8-2));
+VM810:1 24
+undefined
+console.log("Hello," + "World !");
+console.log(((-1)*(-10)*(-20));
+console.log(2*(4+5*(6+3)));
+consolo.log(36/3+4*3);
+VM1184:2 Uncaught SyntaxError: missing ) after argument list
+console.log("Hello," + "World !");
+console.log((-1)*(-10)*(-20));
+console.log(2*(4+5*(6+3)));
+consolo.log(36/3+4*3);
+VM1197:1 Hello,World !
+VM1197:2 -200
+VM1197:3 98
+VM1197:4 Uncaught ReferenceError: consolo is not defined
+    at <anonymous>:4:1
+(anonymous) @ VM1197:4
+console.log("Hello," + "World !");
+console.log((-1)*(-10)*(-20));
+console.log(2*(4+5*(6+3)));
+console.log(36/3+4*3);
+VM1215:1 Hello,World !
+VM1215:2 -200
+VM1215:3 98
+VM1215:4 24
+undefined
